807c4c788c4c064a2245948ca8dee6eedc3c3eba
Adrian Cosma <adriancosma_en@yahoo.com>
2015-09-14 12:32:25 +0300

    thread edit

--- src/main/java/ocp/threads/ThreadTest.java
+++ src/main/java/ocp/threads/ThreadTest.java
@@ -3,5 +3,42 @@ package ocp.threads;
 public class ThreadTest {
 	public static void main (String ... args) {
 		System.out.println(new Runnable() {public void run() {}});
+		
+		
+		/**
+		 * Thread priorities
+		 * 
+		 * -----
+		 * Not all JVMs have the same priority levels
+		 */
+		int priority = 0;
+		
+		priority = Thread.MIN_PRIORITY; //1
+		priority = Thread.NORM_PRIORITY; //5
+		priority = Thread.MAX_PRIORITY; //10
+		
+		System.out.println(priority);
+		
+		/**
+		 * Synchronized Blocks
+		 * 
+		 * -----
+		 * If no object is mentioned in a synchronized block,
+		 * the lock will be aquired for this object.
+		 * 
+		 * -----
+		 * In order to have an effect, notify(), must be called
+		 * from a different thread.
+		 * 
+		 */
+		
+		/**
+		 * Thread methods
+		 * 
+		 * -----
+		 * join() throws InterrupedException, thus it must ALWAYS
+		 * handle or throw this exception
+		 * 
+		 */
 	}
 }
